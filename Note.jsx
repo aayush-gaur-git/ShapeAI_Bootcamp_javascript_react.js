e10891699d7f07ea8d0d76a8df6c669afe2a701a
aayush-gaur-git <85409483+aayush-gaur-git@users.noreply.github.com>
2021-06-06 01:44:30 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+// import ReactDOM from "react-dom";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js </h1>
+      <p>This was a amazing bootcamp taken up by Shaurya Sinha
+        WE covered everything from scratch including Javascript React.js, HTML.
+
+      </p>
+    </div>
+  );
+}
+
+export default Note;
